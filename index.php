c4a33e81d93deeed9cc48719500f2087dbacafa3
CSS-Lletya <51089431+CSS-Lletya@users.noreply.github.com>
2021-07-24 17:42:33 -0400

    Updated UI with new features
    Added Toast, Offcanvas, Jumbotron video, responsiveness, more.
    
    Bootstrap 5.0 documentation page link: https://getbootstrap.com/docs/5.0/getting-started/introduction/

--- index.php
+++ index.php
@@ -11,6 +11,29 @@
     <?php
         include 'nav.php';
     ?>
+    <div class="jumbotron jumbotron-fluid">
+     <video autoplay muted loop oncontextmenu="return false;" src="./resources/video.mp4" type="video/mp4"></video>
+     <div class="jumbo-container">
+       <h1>Hello, <?php echo $user->username ?>!</h1>
+       <?php if ($user->id == $configuration->isDev($user->id)): ?>
+         <p class="sm-margin">Welcome to the official open633 Web Panel (Dashboard).<br>As a Developer you have full access to all the contents that the open633 Dashboard has to offer.</p>
+       <?php else : ?>
+         <p class="sm-margin">Welcome to the official open633 Web Panel (Dashboard).<br>Here you may be able to read the latest <a href="notes.php">Staff Notes</a>, view misc information related to the Website or Server, etc...</p>
+       <?php endif ?>
+     </div>
+   </div>
+
+   <div class="toast-container position-fixed bottom-0 end-0 p-3">
+     <div id="welcomeToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+       <div class="toast-header bg-primary text-light">
+         <h5 class="my-0">Welcome, <?php echo $user->username ?></h5> <img class="avatar" src="<?php echo $avatar ?>" title="<?php echo $user->username ?>"></img>
+       </div>
+       <div class="toast-body">
+           <p>You have signed in successfully, if you have any questions, suggestions, etc.. please report them to the <a href="https://discord.gg/zuqCZhHQaG">Discord Server</a>.</p>
+       </div>
+     </div>
+   </div>
+
     <footer>
       <?php
         include 'footer.php';
--- nav.php
+++ nav.php
@@ -6,37 +6,43 @@ if ($_SESSION == null) {
 }
 ?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-<div class="container-fluid">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Dropdown link
-        </a>
-        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <li><a class="dropdown-item" href="#">Action</a></li>
-          <li><a class="dropdown-item" href="#">Another action</a></li>
-          <li><a class="dropdown-item" href="#">Something else here</a></li>
-        </ul>
-      </li>
-    </ul>
-    <form class="d-flex form-inline my-2 my-lg-0">
-        <img class="avatar" src="<?php echo $avatar ?>" title="<?php echo $user->username ?>"></img>
-        <a href="http://localhost/633_web_panel/logout.php" class="btn btn-outline-success my-2 my-sm-0">Logout</a>
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href=""> <img src="http://localhost/633_web_panel/resources/logo.png" class="logo">open633 - Web Panel</a>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Logs</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <button class="btn btn-light" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Profile</button>
       </form>
+    </div>
+  </div>
+</nav>
+
+<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+  <div class="offcanvas-header">
+    <h3>Currently signed in as:</h3>
+    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body text-center">
+    <div class="container">
+      <img class="avatar" src="<?php echo $avatar ?>" title="<?php echo $user->username ?>"></img>
+      <p><?php echo $user->username?></p>
+      <p>Discord ID: <?php echo $user->id ?></p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p><a href="http://localhost/633_web_panel/logout.php" class="btn btn-outline-danger my-2 my-sm-0 position-absolute bottom-10 start-30 translate-middle">Logout</a></p>
+      <ul class="position-absolute bottom-0 start-50 translate-middle-x">
+        <li> <a href="https://github.com/openrsx/">open633 - GitHub</a></li>
+        <li> <a href="https://discord.gg/zuqCZhHQaG">Discord Server</a></li>
+      </ul>
+    </div>
   </div>
 </div>
-</nav>
